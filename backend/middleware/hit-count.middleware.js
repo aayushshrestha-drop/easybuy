b00ba668ef6ab93709bdbd3a824ba6e29119ead4
Aayush Shrestha <aayush.shrestha@drop.com.sg>
2026-05-07 08:27:23 +0545

    ..

--- backend/middleware/hit-count.middleware.js
+++ backend/middleware/hit-count.middleware.js
@@ -3,9 +3,10 @@ const HitCount = require('../models/hit-count.model');
 const trackHit = async (req, res, next) => {
   try {
     const ip =
+      req.headers['x-real-ip'] ||
       req.headers['x-forwarded-for']?.split(',')[0].trim() ||
-      req.socket.remoteAddress ||
       req.ip ||
+      req.socket.remoteAddress ||
       'unknown';
     
     const hitCount = new HitCount({
